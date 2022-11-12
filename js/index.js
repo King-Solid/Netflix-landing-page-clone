d5e1e614cc63782113bb0688ae3483a9b45bb2a0
Solomon Mba <solomon.mba.official@gmail.com>
2022-11-12 11:04:23 +0100

    First State Commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select tab content item
+function selectItem(e) {
+    // Remove all show and border classes
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grab content item from the DOM
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+    // Add Show Class
+    tabContentItems.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border');
+    });
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show');
+    });
+}
+
+
+// Lister for tab click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
